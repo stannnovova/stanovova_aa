77d912dbfe034b45e697ee8f7b93ebc5869d689b
Anna Stanovova <stannnovova@gmail.com>
2025-09-19 09:03:49 +0300

    feat: add numbers input

--- stanovova__.py
+++ stanovova__.py
@@ -12,3 +12,9 @@ def f(x, p):
         return f(x - 4, p + 1) or f(x - 6, p + 1) or f(ceil(x / 2), p + 1)
     else: return f(x - 4, p + 1) and f(x - 6, p + 1) and f(ceil(x / 2), p + 1)
 print([s for s in range(20, 150) if f(s, 0)])
+
+try:
+    a, b = map(int, input("Введите два числа через пробел: ").split())
+    print(f"a = {a}, b = {b}")
+except ValueError:
+    print("Ошибка: введите два целых числа!")
